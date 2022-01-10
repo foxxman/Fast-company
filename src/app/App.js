bebe27c31efd30a9200ef292c1476594c4bf98ba
foxxman <chuprov9977@gmail.com>
2022-01-10 17:10:22 +0300

    add some things

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,22 @@
 import React from "react";
-import Users from "./components/users";
+// import UsersList from "./components/usersList";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Users from "./layout/users";
+import Main from "./layout/main";
+import Login from "./layout/login";
+
 const App = () => {
-  return <Users />;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/users" component={Users} />
+          <Route path="/" exact component={Main} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
